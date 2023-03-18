39c91ecf3300596f9ff9e0c1e340caf7ba85a215
Sebastian <s.hesse@hgs-singen.online>
2023-03-18 17:57:33 +0100

    Kommentare gelöscht

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -3,28 +3,19 @@ package main;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-/*Der KeyHandler ließt Tastendrücke und wertet diese aus.
- *Somit kann man auf Tastendrücke mit einer aktion reagieren wie zum Beispiel Laufen*/
 public class KeyHandler implements KeyListener{
 
-	/*booleans werden für die jeweiligen richtungen oder Tastendrücke erstellt.*/
 	public boolean obenGedrückt,untenGedrückt,linksGedrückt,rechtsGedrückt;
 	
-	
-	/*Bisher nicht benutzt, wenn Taste nur kurz gedrückt.*/
 	@Override
 	public void keyTyped(KeyEvent e) {	
 	}
 
-	/*Wenn eine Taste gedrückt wurde (gehalten) dann soll das passieren:*/
 	@Override
 	public void keyPressed(KeyEvent e) {
 		
-		/*Es wird eine nummer in dem integer gespeichert welche assoziiert mit einer bestimmten taste ist.
-		 *Zum Beispiel: bei Enter -> 10, bei A = 65 usw..*/
 		int tastenCode = e.getKeyCode();
 		
-		/*Wenn die Nummer der eben gedrückten Taste auf die Nummer von z.b.: "W" zutrifft, dann mache das usw..*/
 		if (tastenCode == KeyEvent.VK_W) {
 			obenGedrückt = true;
 		}
@@ -39,15 +30,12 @@ public class KeyHandler implements KeyListener{
 		}
 		
 	}
-	/*Wenn eine Taste losgelassen wurde, dann soll das passieren:*/
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
 		
-		/*Es wird eine nummer in dem integer gespeichert welche assoziiert mit einer bestimmten taste ist.
-		 *Zum Beispiel: bei Enter -> 10, bei A = 65 usw..*/
 		int tastenCode = e.getKeyCode();
 		
-		/*Wenn die Nummer der eben losgelassenen Taste auf die Nummer von z.b.: "W" zutrifft, dann mache das usw..*/
 		if (tastenCode == KeyEvent.VK_W) {
 			obenGedrückt = false;
 		}
