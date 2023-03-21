67a0984589a3926c4e67b0f4abfaef76be19764e
s.hesse <s.hesse@a206-06.ad.singen-hgs.logodidact.net>
2023-03-21 12:30:50 +0100

    feld klassen und pakeges zeug zum 80 mal weils vorher nicht geklappt hat

---
+++ src/felder/Feld.java
@@ -0,0 +1,8 @@
+package felder;
+
+import java.awt.image.BufferedImage;
+
+public class Feld {
+	BufferedImage image;
+	boolean kollision;
+}
---
+++ src/felder/FeldManager.java
@@ -0,0 +1,8 @@
+package felder;
+
+public class FeldManager {
+
+	Feld[] feld;
+	
+	
+}
