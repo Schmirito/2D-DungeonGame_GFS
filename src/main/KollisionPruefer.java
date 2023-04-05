5c78b57b54c108830d15405bbb01653826b4eab6
ntauschel <n.tauschel@hgs-singen.online>
2023-04-05 21:27:01 +0200

    weiter gemacht

--- src/main/KollisionPruefer.java
+++ src/main/KollisionPruefer.java
@@ -10,5 +10,36 @@ public class KollisionPruefer {
 	}
 	public void pruefeFeld(Entity entity) {
 		
+		int hitBoxLinkesWeltX = entity.weltX + entity.hitBox.x;
+		int hitBoxRechtesWeltX = entity.weltX + entity.hitBox.x + entity.hitBox.width;
+		int hitBoxOberesWeltY = entity.weltY + entity.hitBox.y;
+		int hitBoxUnteresWeltY = entity.weltY + entity.hitBox.y + entity.hitBox.height;
+		
+		int hitBoxLinkeSpalte = hitBoxLinkesWeltX/gp.feldGroeﬂe;
+		int hitBoxRechteSpalte = hitBoxRechtesWeltX/gp.feldGroeﬂe;
+		int hitBoxObereReihe = hitBoxOberesWeltY/gp.feldGroeﬂe;
+		int hitBoxUntereReihe = hitBoxUnteresWeltY/gp.feldGroeﬂe;
+		
+		int feldNum1, feldNum2;
+		
+		switch (entity.richtung) {
+		case "oben":
+			
+			break;
+
+		case "unten":
+			
+			break;
+		case "links":
+			
+			break;
+		case "rechts":
+			
+			break;
+		default:
+			
+			break;
+		}
+		
 	}
 }
