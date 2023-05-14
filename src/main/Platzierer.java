42d4a2718baf743833f85f980921dcb24906a087
ntauschel <n.tauschel@hgs-singen.online>
2023-05-15 00:48:57 +0200

    Zombie spawn Position verändert

--- src/main/Platzierer.java
+++ src/main/Platzierer.java
@@ -27,7 +27,7 @@ public class Platzierer {
 		gp.entities[0] = new Zombie(gp, 20 * gp.feldGroeﬂe, 20 * gp.feldGroeﬂe);
 		gp.entities[1] = new Zombie(gp, 18 * gp.feldGroeﬂe, 20 * gp.feldGroeﬂe);
 		gp.entities[2] = new Zombie(gp, 13 * gp.feldGroeﬂe, 10 * gp.feldGroeﬂe);
-		gp.entities[3] = new Zombie(gp, 17 * gp.feldGroeﬂe, 5 * gp.feldGroeﬂe);
+		gp.entities[3] = new Zombie(gp, 17 * gp.feldGroeﬂe, 15 * gp.feldGroeﬂe);
 		gp.entities[4] = new Zombie(gp, 15 * gp.feldGroeﬂe, 20 * gp.feldGroeﬂe);
 		gp.entities[5] = new Zombie(gp, 19 * gp.feldGroeﬂe, 18 * gp.feldGroeﬂe);
 		gp.entities[6] = new Zombie(gp, 15 * gp.feldGroeﬂe, 13 * gp.feldGroeﬂe);
