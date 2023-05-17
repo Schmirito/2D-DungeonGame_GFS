1b6fced11318a516a418f65c58e710a672b3e99a
ntauschel <n.tauschel@hgs-singen.online>
2023-05-17 22:53:33 +0200

    Zombies werden random im Raum platztiert

--- src/main/Platzierer.java
+++ src/main/Platzierer.java
@@ -9,6 +9,8 @@ public class Platzierer {
 
 	GamePanel gp;
 	int indexObjekte;
+	int randomFeldX;
+	int randomFeldY;
 
 	public Platzierer(GamePanel gp) {
 		this.gp = gp;
@@ -25,15 +27,20 @@ public class Platzierer {
 	}
 
 	public void setzeEntity() {
+		randomFeld();
+		gp.entities[0] = new Zombie(gp, randomFeldX * gp.feldGroeﬂe, randomFeldY * gp.feldGroeﬂe);
+		randomFeld();
+		gp.entities[1] = new Zombie(gp, randomFeldX * gp.feldGroeﬂe, randomFeldY * gp.feldGroeﬂe);
+		randomFeld();
+		gp.entities[2] = new Zombie(gp, randomFeldX * gp.feldGroeﬂe, randomFeldY * gp.feldGroeﬂe);
 
-		gp.entities[0] = new Zombie(gp, 20 * gp.feldGroeﬂe, 20 * gp.feldGroeﬂe);
-		gp.entities[1] = new Zombie(gp, 18 * gp.feldGroeﬂe, 20 * gp.feldGroeﬂe);
-		gp.entities[2] = new Zombie(gp, 13 * gp.feldGroeﬂe, 10 * gp.feldGroeﬂe);
-		gp.entities[3] = new Zombie(gp, 17 * gp.feldGroeﬂe, 15 * gp.feldGroeﬂe);
-		gp.entities[4] = new Zombie(gp, 15 * gp.feldGroeﬂe, 20 * gp.feldGroeﬂe);
-		gp.entities[5] = new Zombie(gp, 19 * gp.feldGroeﬂe, 18 * gp.feldGroeﬂe);
-		gp.entities[6] = new Zombie(gp, 15 * gp.feldGroeﬂe, 13 * gp.feldGroeﬂe);
+	}
 
+	public void randomFeld() {
+		do {
+			randomFeldX = (int) (Math.random() * 29 + 0.5);
+			randomFeldY = (int) (Math.random() * 29 + 0.5);
+		} while (gp.feldM.feld[gp.feldM.mapFeldNr[randomFeldX][randomFeldY]].kollision == true);
 	}
 
 	public void setzeAusgang() {
