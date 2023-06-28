776b6b80077c4a25c919bc7e029bcd0ab73d7ad2
s.hesse <s.hesse@b228-70.ad.singen-hgs.logodidact.net>
2023-06-28 12:10:07 +0200

    dg

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -114,7 +114,6 @@ public class GamePanel extends JPanel implements Runnable {
 			
 			player.update();
 			kamera.update();
-			System.out.println(feldM.dungeonRaumAnz);
 			// ENTITIES
 			for (int i = 0; i < objekte.length; i++) {
 				if (entities[i] != null) {
