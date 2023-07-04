f19ac6be74e80abe92ed652a02e3a46e82543716
ntauschel <n.tauschel@hgs-singen.online>
2023-07-04 07:11:33 +0200

    feld und FelM kommentiert

--- src/felder/Feld.java
+++ src/felder/Feld.java
@@ -2,7 +2,6 @@ package felder;
 
 import java.awt.image.BufferedImage;
 
-/**Constructor von Feld objekten. */
 public class Feld {
 	public BufferedImage image;
 	public boolean kollision;
--- src/felder/FeldManager.java
+++ src/felder/FeldManager.java
@@ -42,7 +42,7 @@ public class FeldManager {
 
 	/**
 	 * Konstruktor für Feldmanager. Manche Variablen werden initialisiert, getFeldBild() wird aufgerufen.
-	 * @param gp
+	 * @param gp GamePanel wird für Bidirektionale Assoziation übergeben.
 	 */
 	public FeldManager(GamePanel gp) {
 		this.gp = gp;
@@ -54,8 +54,9 @@ public class FeldManager {
 		getFeldBild();
 	}
 
-	/**Es wird eine mapNr ausgewählt, welche eine verbindung zu einer .txt datei hat.
-	 * Diese angegebene .txt datei wird ausgelesen, skaliert und gezeichnet. */
+	/**Es wird eine mapNr nach einem bestimmten Vorgang ausgewählt, welche für einen Dateipfad zu einer .txt-Datei, gespeichert im Array mapAuswahl[], steht.
+	 * Daraufhin wird die (neu) ausgewählte Map aus der .txt-Datei in das Integer Array mapFeldNr[][] geladen.
+	 */
 	public void loadMap() {
 		switch (mapNr) {
 		case 0: // Startraum implementieren
@@ -228,7 +229,8 @@ public class FeldManager {
 		System.out.println("raumzähler: "+raumZaeler);
 	}
 
-	/**Jeses Feld wird eingelesen, skaliert und die kollisionen festgelegt. */
+	/**Jedes Feld wird mithilfe der Setup-methode eingelesen, skaliert und der Boolean zur Kollision festgelegt. 
+	 */
 	public void getFeldBild() {
 		setup(0, "D001Boden", false);
 		setup(1, "D002Kiste", true);
@@ -288,9 +290,10 @@ public class FeldManager {
 	}
 
 	/**Die Felder werden unter einem Dateipfad gesucht, skaliert und kollisionen hinzugefügt.
-	 * @param index
-	 * @param bildName
-	 * @param kollision */
+	 * @param index	Die Nummer, welche in der .txt-Dateit für dieses Feld steht.
+	 * @param bildName Der Bildname ohne weiteren Pfad.
+	 * @param kollision Ob Kollison mit diesem Block möglich ist.
+	 */
 	public void setup(int index, String bildName, boolean kollision) {
 		UtilityTool uTool = new UtilityTool();
 
@@ -305,7 +308,11 @@ public class FeldManager {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * Gibt den Index für das Feld mit dem übergebenen Bildnamen zurück.
+	 * @param bildName Der Bildname des Felds, für welches der Index gesucht werden soll.
+	 * @return Der Index als Integer-Wert.
+	 */
 	public int getFeldIndex(String bildName) {
 		int index = -1;
 		for (int i = 0; i < feld.length; i++) {
@@ -319,9 +326,9 @@ public class FeldManager {
 		return index;
 	}
 
-	/**Die skalierten Bilder werden an der, ihnen jeweils zugehörigen, mapFeldNr gezeichnet.
-	 * @param g2
-	 *  */
+	/**Die Bilder der jeweiligen Felder werden an der ihnen jeweils zugehörigen Position in der Welt, wenn sich diese auf dem Bildschirm befindet, gezeichnet.
+	 * @param g2 Das Graphics2D-Objekt zum Zeichnen der Felder.
+	 */
 	public void draw(Graphics2D g2) {
 
 		int spalte = 0;
@@ -336,7 +343,6 @@ public class FeldManager {
 				int feldNr = mapFeldNr[spalte][reihe];
 				g2.drawImage(feld[feldNr].image, x, y, gp.feldGroeße, gp.feldGroeße, null);
 			}
-			// g2.drawRect(x, y, gp.feldGroeße, gp.feldGroeße);
 			spalte++;
 			x += gp.feldGroeße;
 
